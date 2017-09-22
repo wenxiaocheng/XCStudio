32f1ab7c903df62580143c2016019d49c75ca029
小成 <634650981@qq.com>
2017-09-22 14:50:18 +0800

    11111

---
+++ src/main/java/mybatis/User.java
@@ -0,0 +1,73 @@
+package mybatis;
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String sex;
+	private String name;
+	private Date birthday;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public User(int id, String username, String password, String sex, String name, Date birthday) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.name = name;
+		this.birthday = birthday;
+	}
+
+	public User() {
+		super();
+	}
+}
---
+++ src/main/java/mybatis/UserMapper.java
@@ -0,0 +1,7 @@
+package mybatis;
+
+public interface UserMapper {
+	public void insertUser(User user);
+
+	public User getUser(String name);
+}
