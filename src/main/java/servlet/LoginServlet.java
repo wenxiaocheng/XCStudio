d28ccb8e75008a1fa56290fe49d18ef5219097e7
小成 <634650981@qq.com>
2017-09-23 19:00:30 +0800

    update

---
+++ src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if (username.equals("wen") && password.equals("xiaocheng")) {
+			response.sendRedirect("/hk-jsp-study/success.jsp");
+		} else {
+			response.sendRedirect("/hk-jsp-study/error.jsp");
+		}
+	}
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>登录</title>
+</head>
+<body>
+	<form action="LoginServlet" method="post">
+		用户名：<input type="text" name="username" title="用户名"><br>
+		密&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="password"
+			title="密码"><br> <input type="submit" value="登录">
+		<input type="reset" value="重置">
+	</form>
+</body>
+</html>
