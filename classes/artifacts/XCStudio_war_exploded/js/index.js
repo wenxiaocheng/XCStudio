8d7bac326b2eff10ab0f8fd229a9df8c7d3103bd
小成 <634650981@qq.com>
2017-10-08 19:46:59 +0800

    update

--- classes/artifacts/XCStudio_war_exploded/js/index.js
+++
@@ -1,4 +0,0 @@
-$(Document).ready(function () {
-    // 幻灯片
-    $('#carousel-index').carousel();
-});
--- classes/artifacts/XCStudio_war_exploded/js/module/footer.js
+++
@@ -1,4 +0,0 @@
-$(Document).ready(function () {
-    // 二维码分享
-    $("#share-accordion").accordion();
-});
